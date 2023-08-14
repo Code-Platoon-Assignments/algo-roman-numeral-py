81e8ec3694b6563a1d85e7cee992b7d80b91f024
fravila08 <fr4v1l4@gmail.com>
2023-08-14 15:10:41 -0700

    done

---
+++ roman_numerals.py
@@ -0,0 +1,24 @@
+def to_roman(num):
+    # write your code here!
+    numerals ={
+        "M" : 1000,
+        "CM": 900,
+        "D" : 500,
+        "CD": 400,
+        "C" : 100,
+        "XC": 90,
+        "L" : 50,
+        "XL": 40,
+        "X" : 10,
+        "IX": 9,
+        "V" : 5,
+        "IV": 4,
+        "I" : 1,
+    }
+    answer = ""
+    for numeral, val in numerals.items():
+        if num >= val:
+            times = num//val
+            answer += numeral * times
+            num = num % val
+    return answer
